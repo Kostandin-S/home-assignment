da4cef0f98cfe946324ac756f7914edac81d3c85
Kostandin-S <kostandin.dervishaj@sigma.software>
2025-04-03 09:54:11 +0200

    Installed and configured chakra UI

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,13 @@ import { createRoot } from 'react-dom/client'
 import '@ant-design/v5-patch-for-react-19';
 import './index.css'
 import App from './App.tsx'
+import { Provider } from './components/ChakraUI/provider.tsx';
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <Provider enableSystem={false}>
+      <App />
+    </Provider>
   </StrictMode>,
 )
