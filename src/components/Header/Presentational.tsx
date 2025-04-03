04f40a0cd4277d228fc28339cafcc1eaa78b589b
Kostandin-S <kostandin.dervishaj@sigma.software>
2025-04-03 10:04:04 +0200

    Changed header component implementation from antd to chakra

--- src/components/Header/Presentational.tsx
+++ src/components/Header/Presentational.tsx
@@ -1,17 +1,18 @@
-import { Button, Typography } from 'antd';
-import styles from './styles.module.scss';
+import { Button, Text, Heading, Box } from '@chakra-ui/react';
+import styles from './styles.module.scss'
 
 const Header = () => {
-  const { Title, Text } = Typography;
   return (
-    <div className={styles.container}>
-      <div className={styles.textContainer}>
-        <Title className={styles.text} level={3}>EVINCED</Title>
-        <Text className={styles.text} >Production Monitoring DEV</Text>
-      </div>
-      <Button>Log out</Button>
-    </div>
-  )
+    <nav aria-label="Main navigation">
+      <Box className={styles.container}>
+        <Box className={styles.textContainer}>
+          <Heading className={styles.text} as="h3" size="lg">EVINCED</Heading>
+          <Text className={styles.text}>Production Monitoring DEV</Text>
+        </Box>
+        <Button colorPalette="gray" variant="surface" aria-label="Log out" onClick={() => console.log("Logged out")}>Log out</Button>
+      </Box>
+    </nav>
+  );
 };
 
 export default Header;
