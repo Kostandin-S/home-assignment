306c7d8519e0e2f65a9dcd7e689e3fdb5ccdb9f4
Kostandin-S <kostandin.dervishaj@sigma.software>
2025-04-03 10:04:15 +0200

    Used semantic html

--- src/screens/ScanResults/Presentational.tsx
+++ src/screens/ScanResults/Presentational.tsx
@@ -11,10 +11,10 @@ const ScanResults = () => {
   const [selectedCategory, setSelectedCategory] = useState<string>();
 
   return (
-    <div className={styles.container}>
+    <section className={styles.container}>
       <SiteTree analyserResults={analyserResults} setSelectedCategory={setSelectedCategory} selectedCategory={selectedCategory} />
       <ResultsTable analyserResults={analyserResults} selectedCategory={selectedCategory} />
-    </div>
+    </section>
   )
 }
 
